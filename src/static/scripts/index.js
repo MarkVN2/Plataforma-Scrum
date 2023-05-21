4f7e53ed88f8d543a43226788e84cf0f2109d3c4
Vinicius Forcato <76211125+nininhosam@users.noreply.github.com>
2023-05-21 20:20:05 -0300

    Added navbar animation, grouped scattered Scripts.

---
+++ src/static/scripts/index.js
@@ -0,0 +1,26 @@
+// Bootstrap tooltips activation
+const tooltips = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+tooltips.forEach(t=>{new bootstrap.Tooltip(t)});
+
+// Loading screen
+var timerTime;
+function timer(){
+    timerTime = setTimeout(showPage, 1050)
+};
+function showPage(){
+    document.getElementById('loader').style.display = "none";
+};
+
+// Navbar display on scroll:
+let prevScrollpos = window.pageYOffset;
+document.addEventListener("scroll", ()=>{
+  let currentScrollPos = window.pageYOffset
+  let navbar = document.querySelector("nav.navbar")
+  let navHeight = navbar.getBoundingClientRect().height
+  if (prevScrollpos > currentScrollPos) {
+    navbar.style.top = "0";
+  } else {
+    navbar.style.top = `-${navHeight}px`;
+  }
+  prevScrollpos = currentScrollPos
+})
