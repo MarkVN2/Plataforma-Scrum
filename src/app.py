22476effcd2b28195aa4d1a46f5bf754213e09bf
Vinicius Forcato <76211125+nininhosam@users.noreply.github.com>
2023-05-21 21:37:02 -0300

    Integrated home page into "template.html"

--- src/app.py
+++ src/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 # Normal routes
 @app.route("/")
 def index():
-    return render_template('home.html')
+    return render_template('home.html', home=True)
 
 @app.route("/metodologia/introducao")
 def metInt():
