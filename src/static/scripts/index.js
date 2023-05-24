93145ac65439bb6420278b7e1fd3a8061e014507
Vinicius Forcato <76211125+nininhosam@users.noreply.github.com>
2023-05-23 21:58:56 -0300

    Major skim through pages, implementing themes.

--- src/static/scripts/index.js
+++ src/static/scripts/index.js
@@ -14,13 +14,32 @@ function showPage(){
 // Navbar display on scroll:
 let prevScrollpos = window.pageYOffset;
 document.addEventListener("scroll", ()=>{
-  let currentScrollPos = window.pageYOffset
+  let currentScrollPos = window.pageYOffset;
   let navbar = document.querySelector("nav.navbar")
-  let navHeight = navbar.getBoundingClientRect().height
+  let navHeight = navbar.getBoundingClientRect().height;
   if (prevScrollpos > currentScrollPos) {
     navbar.style.top = "0";
   } else {
     navbar.style.top = `-${navHeight}px`;
   }
-  prevScrollpos = currentScrollPos
+  prevScrollpos = currentScrollPos;
+})
+
+// Handling Dark-mode and Light-mode
+let themePreference = localStorage.getItem("themePref")
+let themeSwitch = document.querySelector("input#themeSwitch");
+docRoot = document.documentElement
+if (themePreference == "dark") {
+  docRoot.setAttribute("data-theme", "dark")
+  themeSwitch.checked = true;
+}
+
+themeSwitch.addEventListener("change", el => {
+  if (el.target.checked){
+    docRoot.setAttribute("data-theme", "dark")
+    localStorage.setItem("themePref", "dark")
+  } else {
+    docRoot.setAttribute("data-theme", "light")
+    localStorage.setItem("themePref", "light")
+  }
 })
