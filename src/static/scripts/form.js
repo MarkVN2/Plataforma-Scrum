062a2410d5d1eb6a69f0c0b72e11108d8ecbda1f
MarkVN2 <markosvinicius.nunes@gmail.com>
2023-08-13 16:16:26 -0300

    Adding randomness to the questionary
    Questions are now randomized everytime the page is reloaded to prevent user from just memorizing the anwsers.

--- src/static/scripts/form.js
+++ src/static/scripts/form.js
@@ -2,14 +2,30 @@ import data from '../json/exercises.json' assert { type: 'json' };
 
 //Get ID of area where the forms will go
 const FormArea = document.getElementById('form-container');
-let form_num = data.length; //change this if you don't want the size to be the same as in the JSON
+let form_num = data.length; //change this if you don't want the size to be the same as in the JSON but don't go over the size of the JSON
 let count = 0;
+let alreadyused = [];
+let exercise = Math.floor(Math.random() * data.length);
 
+
+if (form_num > data.length){
+    console.log("|| PREVENTED INFINITE LOOP || DECREASE THE FORM_NUM VARIABLE TO A SIZE WITHIN THE LIMITS OF THE JSON");
+    form_num = data.length;
+}
 
 for (;count<form_num;count++){
+    
+        
+    if (alreadyused.includes(exercise)){
 
-    // let exercise = Math.floor(Math.random() * data.length)     
-    // console.log(exercise) 
+        // console.log(count,exercise)
+        // console.log(alreadyused)
+        // console.log(`${exercise} is already in`)
+
+        exercise = Math.floor(Math.random() * data.length) 
+        count = count - 1
+    }
+    else{
 
     let FormPlace = document.createElement("div");
     FormPlace.id = count;
@@ -17,13 +33,13 @@ for (;count<form_num;count++){
     FormArea.appendChild(FormPlace);
 
     let Header = document.createElement("h1");
-    Header.innerText = String(data[count].exercise_text);
+    Header.innerText = String(data[exercise].exercise_text);
     FormPlace.appendChild(Header);
 
-    for (let key in data[count].anwsers){
+    for (let key in data[exercise].anwsers){
 
         let Label = document.createElement("label");
-        Label.innerHTML = String(data[count].anwsers[key]);
+        Label.innerHTML = String(data[exercise].anwsers[key]);
         Label.className = "form-label";
 
         let Form = document.createElement("input");
@@ -32,23 +48,33 @@ for (;count<form_num;count++){
         Form.type = "radio";
 
         let FormSpan = document.createElement("span");
-        FormSpan.className = "form-span"
+        FormSpan.className = "form-span";
         Form.appendChild(FormSpan);
 
         FormPlace.appendChild(Label);
         Label.appendChild(Form);     
-
+        
+        
     };
-    
+
+
+        // console.log(count,exercise)   
+        // console.log("CREATED")
+        alreadyused.push(exercise)
+        // console.log(alreadyused)
+        
+    };
 };
 
 const submitButton = document.getElementById('submit-btn');
 
 submitButton.onclick = function(){
     let count = 0;
-    data.forEach(question => {
+    for (;count<alreadyused.length;count++){
 
-        let correct_answer = question.anwser;
+        // console.log(alreadyused[count])
+
+        let correct_answer = data[alreadyused[count]].anwser;
         
         let response;
 
@@ -56,18 +82,18 @@ submitButton.onclick = function(){
             input.disabled=true;
             if(input.checked == true){
                 response = input.value;
-                
+                // console.log(response,correct_answer)
             };
         });
         
         if (response == correct_answer){
-            console.log(`${count+1} está correta`)
+            // console.log(`${count+1} está correta`)
             document.getElementById(count).className +=  ' right';
         }
         else{
             document.getElementById(count).className += ' wrong';
         };
-        count++;
-    });
+    
+    };
     this.disabled = true;   
 };
