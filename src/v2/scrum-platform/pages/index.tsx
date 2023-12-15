d09b06a872aa8990311c2b273df51c11ba6ed693
Markos Nunes <105757405+MarkVN2@users.noreply.github.com>
2023-12-15 18:34:03 -0300

    [setup]: Next.js for 2.0

---
+++ src/v2/scrum-platform/pages/index.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import '../src/app/globals.css'
+import Navbar from '@/src/app/modules/navbar'
+
+
+export default function Home() {
+  return (
+    <main>
+      <Navbar/>
+    </main>
+  )
+}
---
+++ src/v2/scrum-platform/src/app/modules/navbar/index.tsx
@@ -0,0 +1,11 @@
+
+import '../../globals.css'
+
+const Navbar = () => {
+    return(
+        <nav className=" bg-transparent  backdrop-blur-sm max-w-screen h-16 border-solid border-b-[0.25px] border-slate-300">
+            <div className='grid-flow-col'></div>
+        </nav>
+    )
+}
+export default Navbar;
